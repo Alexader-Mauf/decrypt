f7a0f8105c3e2cdd3c326a9a807ae6540c92d88e
Alexander Mauf <maufalexander@gmail.com>
2021-01-12 09:21:48 +0100

    errorhandling and trying to get the script to an external location

--- key/static/js/read_write_data.js
+++ key/static/js/read_write_data.js
@@ -1,3 +1,28 @@
-$.post( "ajax/test.html", function( data ) {
-  $( ".result" ).html( data );
-});
+$( "#decode" ).click(function() {
+        var messageText = $("#message").val()
+        $.getJSON( "/key/api/secret-msgs/"+messageText+"/" )
+        .done(function(data) {
+            console.log(data)
+            $("#status-message").text("Deine Nachricht ist:")
+            $("#content").text(data.message)
+            $("#message").val("")
+        })
+        .fail(function(data) {
+            $("#status-message").text("Hoppla")
+            $("#content").text("diese Nachricht scheint nicht zu existieren")
+        })
+
+$( "#encode" ).click(function() {
+    var messagText = $("#message").val()
+    $.post( "/key/api/secret-msgs/" , { "message": messagText })
+     .done(function( data ) {
+        console.log( data );
+        $("#content").text(data.uuid)
+        $("#status-message").text("Erfolgreich gespeichert unter:")
+        $("#message").val("")
+     })
+     .fail(function(data) {
+            $("#status-message").text("Hoppla")
+            $("#content").text("da ist wohl etwas schiefgegangen")
+     })
+

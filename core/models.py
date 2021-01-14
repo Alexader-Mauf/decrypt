8af91923f4e94f9044e95a0f6af24ce3e9a22c76
Alexander Mauf <maufalexander@gmail.com>
2021-01-14 16:12:37 +0100

    started some imports for new bank app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+import datetime
+from django.utils import timezone
+import uuid
 
 # Create your models here.
+
+class SecretMsg(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    message = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.message
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() -datetime.timedelta(days=1)
+
+
+
+class BankModel(models.Model):
+    name = models.TextField(max_length=255)
+    vorname = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    balance = models.CharField(max_length=255)
+
+

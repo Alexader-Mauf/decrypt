3a367f442ce4f1db2c217177489c303f1c7fd700
Alexander Mauf <maufalexander@gmail.com>
2021-01-18 11:04:29 +0100

    added views for logging in and creating a user

--- bank/views.py
+++ bank/views.py
@@ -11,12 +11,47 @@ from . import serializers
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
+from django.auth import User
+from django.contrib.auth import  authenticate
+
 def login(request):
     return render(request, "login.html")
 
 
 def index(request):
-    return render(request, "homescreen.html")
+    # getting usernames as post from login
+    username = "" #request.POST.get("username")
+    password = "" #request.POST.get("password")
+
+
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        return render(request, "homescreen.html")
+    else:
+        return rrender(request, "login.html",{
+            "statusmsg":"ungültige Zugangsdaten",
+        })
+
+
+def createuser(request):
+    # get data from a form
+    vorname = ""
+    nachname = ""
+    email = ""
+    password = ""
+    passwordcheck = ""
+    if password != passwordcheck:
+        return render(request, "signup.html",{
+            "statusmsg" : "Passwörter stimmen nicht überein.",
+        })
+    user  = User.objects.create_user(vorname,email,password)
+    user.last_name = nachname
+    user.save()
+    return render(request, "signup.html", {
+        "statusmsg": "Nutzer erfolgreich erstellt.",
+    })
+
+
 
 
 

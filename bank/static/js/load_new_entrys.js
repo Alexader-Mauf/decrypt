2ce9acbd3393534899eb0e7721252de4a2b2e4ba
Alexander Mauf <maufalexander@gmail.com>
2021-02-26 11:16:55 +0100

    fixed rounding issue

--- bank/static/js/load_new_entrys.js
+++ bank/static/js/load_new_entrys.js
@@ -13,6 +13,8 @@ $(document).ready(function () {
 
   console.log(ibans);
 
+
+
   function round(value, exp) {
      if (typeof exp === 'undefined' || +exp=== 0)
         return Math.round(value);
@@ -35,22 +37,24 @@ $(document).ready(function () {
     $.getJSON(
       "/bank/api/bank-transfers/?ordering=-created_at",
       function (data) {
-        console.log(data);
-        // die folgende Zeile ist vergleichbar mit: "for el in data.results:"
+        // console.log(data);
         var own_iban = $.getJSON(
           "/bank/api/bank-transfers/?ordering=-created_at",
+          // die folgende Zeile ist vergleichbar mit: "for el in data.results:"
           data.results.forEach((el) => {
             var row = `
          <li class="list-group-item d-flex justify-content-between lh-sm">
          <div>
-         <h6 class="my-0">el.use_case</h6>
+         <h6 class="my-0">${el.use_case}</h6>
          <small class="text-muted">Datum der Überweisung: ${el.execute_datetime}</small>
          <br>
-         <small class="text-muted">Absender: ${el.iban_from_username}
-                        ${el.iban_from} </small>
+         <small class="text-muted">Absender: ${el.iban_from_username}</small>
          <br>
-         <small class="text-muted">Empfänger: ${el.iban_to_username}
-             ${el.iban_to}</small>
+         <small class="text-muted">${el.iban_from} </small>
+         <br>
+         <small class="text-muted">Empfänger: ${el.iban_to_username}</small>
+         <br>
+         <small class="text-muted">${el.iban_to}</small>
          <small class="text-muted">Status:</small>`;
             if (el.is_open === false) {
               if (el.is_success === false) {
@@ -67,9 +71,9 @@ $(document).ready(function () {
             }
          row = row + `</div>`
             if (ibans.includes(el.iban_to)) {
-              row = row + `<span class="text-success">+ ${round(el.amount,2)}€</span>`;
+              row = row + `<span class="text-success">+ ${round(el.amount,2).toFixed(2)}€</span>`;
             } else {
-              row = row + `<span class="text-danger">- ${round(el.amount,2)}€</span>`;
+              row = row + `<span class="text-danger">- ${round(el.amount,2).toFixed(2)}€</span>`;
             }
             $("#transactions").append(row);
           })

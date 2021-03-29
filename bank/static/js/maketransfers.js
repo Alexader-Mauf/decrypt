a98d4666206b0919bbf9cca7e205cd8baa37c295
Alexander Mauf <maufalexander@gmail.com>
2021-03-29 14:09:36 +0200

    deleted old code remnants rewrote the maketransfers on change still is anoying

--- bank/static/js/maketransfers.js
+++ bank/static/js/maketransfers.js
@@ -1,22 +1,28 @@
-//$( document ).ready(function() {
-//        var accountValues = null;
-//
-//        $.get( "/api/bank-accounts", function( data ) {
-//          var accounts = data[""]
-//
-//          accountValues = {
-//          "iban": "kontostand"
-//          }
-//          alert( "Load was performed." );
-//        });
-//
-//        $.change("amount"){
-//            selecetIban = $("#iban_from option:selected").text();
-//            if(accountValues[iban]<$("#id_amount").val()){
-//                alert("EVTL nicht genug guthaben")
-//            };
-//            }
-//            // load iban ausm dict
-//            // kontostand < ueberweisungssumme
-//            // alert ausgeben
-//});
+$( document ).ready(function() {
+        var accountValues = null;
+
+        $.get( "/bank/api/bank-accounts/", function( data ) {
+          var accounts = data[""];
+
+          accountValues = {
+          "iban": "kontostand"
+          }
+          console.log( "Load was performed." );
+          console.log($("#iban_from option:selected").text());
+        });
+
+
+            // load iban ausm dict
+            // kontostand < ueberweisungssumme
+            // alert ausgeben
+});
+$("amount").change(console.log("spam "))
+
+
+$("amount").change(function(){
+    selecetIban = $("#iban_from option:selected").text();
+    console.log("change");
+    if(accountValues[selecetIban]<$("#id_amount").val()){
+        alert("EVTL nicht genug guthaben")
+    };
+    });

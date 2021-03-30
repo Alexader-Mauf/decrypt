2b36c44048489656dcb477f09ae229824fcc2814
Alexander Mauf <maufalexander@gmail.com>
2021-03-30 09:06:06 +0200

    docker compose: added volumeisation urls: fixed the displayed url of keyform not actually being callable

--- keyform/urls.py
+++ keyform/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('create-secretnote', views.create_secretnote, name='create_secretnote'),
     path('encode-decode', views.encode_decode, name='encode_decode'),
     path('home/<uuid>/', views.load_secretnote, name='load_secretnote'),
+    path('home/<uuid>', views.load_secretnote, name='load_secretnote'),
     path('decode', views.decode, name='decode'),
     path('apistyles', apiviews.index, name='apistyle'),
     path('banking', banking.index, name='banking'),
